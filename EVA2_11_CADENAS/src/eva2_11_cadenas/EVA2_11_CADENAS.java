4a77601547776ac7e4521364b7a2fb232fdfae47
ihdzg <143562753+ihdzg@users.noreply.github.com>
2023-10-18 17:50:49 -0600

    Add files via upload

---
+++ EVA2_11_CADENAS/src/eva2_11_cadenas/EVA2_11_CADENAS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_11_cadenas;
+
+/**
+ *
+ * @author ihdzg
+ */
+public class EVA2_11_CADENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String cade = "HOLA";
+        
+        if(cade.equals("HOLA"))
+            System.out.println("Las cadenas son iguales");
+        else
+            System.out.println("Las cadenas no son iguales");
+    }
+    
+}
